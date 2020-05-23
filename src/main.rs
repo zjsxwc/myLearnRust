8d1c059fa15eac49cfc22fd014c1034e6eb80db8
wangchao <watcher.hangzhou@foxmail.com>
2020-05-23 17:30:43 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,43 @@ fn p(name: &str) {
     println!("Hello, {}", name);
 }
 
+
+
+
+
+
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+
+
+
 fn main() {
+
+
+    //Rc Week 每次new、downgrade、upgrade 就是不断套马甲
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+
+
+
+
+
+
     let x = 5;
     let y = MyBox::new(0, x);
     assert_eq!(5, x);
